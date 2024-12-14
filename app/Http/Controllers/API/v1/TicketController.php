26eb6040fd013a8dbb6b9f2b8e6821e862e6fef0
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-12-14 12:23:50 +0200

    Initial commit

---
+++ app/Http/Controllers/API/v1/TicketController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Ticket;
+use App\Http\Requests\API\v1\StoreTicketRequest;
+use App\Http\Requests\API\v1\UpdateTicketRequest;
+
+class TicketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Ticket::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTicketRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ticket $ticket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTicketRequest $request, Ticket $ticket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ticket $ticket)
+    {
+        //
+    }
+}
